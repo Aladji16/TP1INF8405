23e897a315a1d33b7da0be552322b33ba5da9f09
Philippe Duong <philippeduoong495@gmail.com>
2021-02-08 19:18:53 -0500

    Added exit onClickListener, WIP for play and about

--- app/src/main/java/com/example/tp1inf8405/MainActivity.java
+++ app/src/main/java/com/example/tp1inf8405/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.tp1inf8405;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button playB = findViewById(R.id.playButton);
+        Button aboutB = findViewById(R.id.aboutButton);
+        Button exitB = findViewById(R.id.exitButton);
+
+        playB.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getApplicationContext(), "You chose PLAY!", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+
+        aboutB.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getApplicationContext(), "You chose ABOUT!", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+
+        exitB.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                // Exit the application
+                finish();
+                System.exit(0);
+            }
+        });
     }
 }
