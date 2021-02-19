fa8efe480126f6d9ebffddff11614e4cf55ec23a
Aladji16 <aladji.faye@polymtl.ca>
2021-02-19 15:36:50 -0500

    Ajout choix taille de grille

--- app/src/main/java/com/example/tp1inf8405/ChooseGridSize.java
+++ app/src/main/java/com/example/tp1inf8405/ChooseGridSize.java
@@ -2,13 +2,36 @@ package com.example.tp1inf8405;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
-public class ChooseNumberOfLinesActivity extends AppCompatActivity {
+
+public class ChooseGridSize extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_choose_number);
+        setContentView(R.layout.activity_size);
+
+        Button confirmButton = findViewById(R.id.buttonConfirm);
+        RadioGroup radioGroup = findViewById(R.id.radioSizeGroup);
+
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+//                Intent aboutIntent = new Intent(getApplicationContext(), AboutActivity.class);
+//                startActivity(aboutIntent);
+                RadioButton checkedButton = findViewById(radioGroup.getCheckedRadioButtonId());
+
+                Toast toast = Toast.makeText(getApplicationContext(), "You chose "+ checkedButton.getText(), Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
     }
 }
