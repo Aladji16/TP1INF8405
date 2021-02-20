a0a7261dfdb38719a21064b1587d4c61b1fe57b8
Aladji16 <aladji.faye@polymtl.ca>
2021-02-20 12:47:48 -0500

    le joueur qui a gagné est bien affiché

--- app/src/main/java/com/example/tp1inf8405/ButtonToeClass.java
+++ app/src/main/java/com/example/tp1inf8405/ButtonToeClass.java
@@ -1,4 +1,44 @@
 package com.example.tp1inf8405;
 
-public class ButtonToeClass {
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.GridView;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ButtonToeClass extends ImageButton {
+    private int player; //si le joueur 1 ou 2 a joué (débute à 0)
+    private int position; //position du bouton
+
+    public ButtonToeClass(Context context, int position)
+    {
+        super(context);
+        this.position = position;
+        player = 0;
+    }
+
+
+    public int getPosition()
+    {
+        return this.position;
+    }
+
+    public void setPlayer(int player)
+    {
+        this.player = player;
+    }
+
+    public int getPlayer()
+    {
+        return this.player;
+    }
+
 }
--- app/src/main/java/com/example/tp1inf8405/GameActivity.java
+++ app/src/main/java/com/example/tp1inf8405/GameActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridLayout;
@@ -15,9 +16,100 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.HashMap;
+
 public class GameActivity extends AppCompatActivity {
 
     public int compteur = 0;
+
+
+    public int isGameOver(HashMap<Integer,ButtonToeClass> positionMap, ButtonToeClass lastPlay, int gridSize)
+            //retourne 0 si personne n'a gagné, 1 si c'est le joueur 1, 2 si c'est le joueur 2
+    {
+
+        int lastPlayer = lastPlay.getPlayer();
+        int lastPlayPosition = lastPlay.getPosition();
+
+
+        int row = lastPlayPosition / gridSize;
+        int col = lastPlayPosition % gridSize;
+
+
+        //vérification ligne
+        int i = row;
+        ButtonToeClass button = positionMap.get(i);
+        while ((i < row + gridSize) && button.getPlayer() == lastPlayer)
+        {
+            i++;
+            button = positionMap.get(i);
+        }
+        if (i == row + gridSize)
+        {
+            return lastPlayer;
+        }
+
+
+
+
+        //vérification colonne
+        i = col;
+        button = positionMap.get(i);
+        while ( (i < col + gridSize * gridSize) && button.getPlayer() == lastPlayer)
+        {
+            i += gridSize;
+            button = positionMap.get(i);
+        }
+
+        if (i == col + gridSize * gridSize)
+        {
+            return lastPlayer;
+        }
+
+
+
+        //vérification diagonale "gauche"
+        if (row == col)
+        {
+            i = 0;
+            button = positionMap.get(i);
+
+            while (i < gridSize*(gridSize + 1) &&  button.getPlayer() == lastPlayer)
+            {
+                i += gridSize+1;
+                button = positionMap.get(i);
+
+            }
+            if (i == gridSize*(gridSize + 1))
+            {
+                return lastPlayer;
+            }
+
+        }
+
+
+        //vérification diagonale "droite"
+        if (row + col == gridSize - 1)
+        {
+
+            i = gridSize - 1;
+            button = positionMap.get(i);
+
+            while (i < gridSize*gridSize - 1 &&  button.getPlayer() == lastPlayer)
+            {
+                i += gridSize-1;
+                button = positionMap.get(i);
+
+            }
+            if (i == gridSize*gridSize - 1)
+            {
+                return lastPlayer;
+            }
+
+        }
+
+        return 0;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +118,8 @@ public class GameActivity extends AppCompatActivity {
         setContentView(R.layout.activity_game);
 
         TextView textViewTurn = (TextView) findViewById(R.id.textViewTurn);
+
+        HashMap<Integer,ButtonToeClass> positionMap = new HashMap<Integer,ButtonToeClass>(); //hashmap pour lier les positions aux boutons
 
 
 
@@ -36,34 +130,51 @@ public class GameActivity extends AppCompatActivity {
             gridSize = Integer.parseInt(i.getCharSequenceExtra("size").toString());
         }
 
+
         androidx.gridlayout.widget.GridLayout gameGrid = findViewById(R.id.gameGrid);
         gameGrid.setColumnCount(gridSize);
         gameGrid.setRowCount(gridSize);
 
         //        code pour afficher une croix
         for (int j = 0; j < gridSize * gridSize; j++) {
-            ImageButton imageButton = new ImageButton(getApplicationContext());
+            ButtonToeClass imageButton = new ButtonToeClass(getApplicationContext(),j);
+            positionMap.put(j, imageButton);
+
+
+            int finalGridSize = gridSize;
             imageButton.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View view) {
-//                Toast toast = Toast.makeText(getApplicationContext(), "You chose PLAY!", Toast.LENGTH_LONG);
-//                toast.show();
+
+
+
                     compteur++;
-                    imageButton.setAdjustViewBounds(true);
+                    imageButton.setAdjustViewBounds(true); //pour pouvoir modifier la taille de l'image
                     imageButton.setBackgroundColor(Color.WHITE);
 
                     CharSequence text = "Tour du joueur 1";
                     if (compteur % 2 == 1)
-                    {
+                    { //o
+                        imageButton.setPlayer(1);
+//                        imageButton.setImageResource(R.drawable.post_97990_1260678636);
                         text = "Tour du joueur 2";
-                        imageButton.setImageResource(R.drawable.post_97990_1260678617);
+
                     }
 
                     else
-                    {
-                        imageButton.setImageResource(R.drawable.post_97990_1260678636);
+                    { //x
+                        imageButton.setPlayer(2);
+//                        imageButton.setImageResource(R.drawable.post_97990_1260678617);
+
                     }
                     textViewTurn.setText(text);
                     imageButton.setClickable(false);
+
+                    int winner = isGameOver(positionMap, imageButton, finalGridSize);
+                    if (winner > 0)
+                    {
+                        Toast toast = Toast.makeText(getApplicationContext(), "Le joueur "+ String.valueOf(winner) + " a gagné", Toast.LENGTH_LONG);
+                        toast.show();
+                    }
 
 
                 }
